4cc91e25f0199a465997bb2aabb3071c2a8935ef
camyllasantos <camyllastudys@gmail.com>
2025-03-17 08:38:41 -0300

    Fix: correções nos atributos

--- src/com/java/TesteConta.java
+++ src/com/java/TesteConta.java
@@ -2,5 +2,30 @@ package com.java;
 
 public class TesteConta {
 
-    
+    public static void main(String[] args) {
+
+//       A palavra "new" em Java gera um objeto em memoria
+
+        Conta contaCorrente = new Conta();
+        Conta contaPoupanca = new Conta();
+        Conta contaInvestimento = new Conta();
+
+        contaCorrente.numero = 11;
+        contaCorrente.saldo = 350;
+        contaCorrente.cliente.nome = "Java";
+
+        contaPoupanca.numero = 500;
+        contaPoupanca.saldo = 100;
+        contaPoupanca.cliente.nome = "Leo";
+
+        contaInvestimento.numero = 63;
+        contaInvestimento.saldo = 2;
+        contaInvestimento.cliente.nome = "Mila";
+
+        System.out.println(contaCorrente.numero);
+        System.out.println(contaCorrente.saldo);
+        System.out.println(contaCorrente.cliente.nome);
+
+    }
+
 }
